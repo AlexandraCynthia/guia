be1898721fcab933684e919935d9b1dcb9d411cf
unknown <alexandracynthia1663@gmail.com>
2023-09-28 14:08:15 -0500

    my first commit

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,28 @@
+import Proj1 from "../assets/proj1.jpg";
+import Proj2 from "../assets/proj2.jpeg";
+import Proj3 from "../assets/proj3.jpg";
+import Proj4 from "../assets/proj4.jpeg";
+
+export const ProjectList = [
+  {
+    name: "Dijkstra Algorithm Visualizer",
+    image: Proj1,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Ecommerce Website",
+    image: Proj2,
+    skills: "React,Node.js,MongoDB",
+  },
+  {
+    name: "Spotify Clone",
+    image: Proj3,
+    skills: "React,Node.js,MongoDB,SpotifyAPI",
+  },
+  {
+    name: "Social Media Website",
+    image: Proj4,
+    skills: "React,Node.js,MySQL,GraphQL",
+  }
+  
+];
